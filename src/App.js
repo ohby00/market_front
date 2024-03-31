d07e04fdd165b60dd0efcb3f0a221c014dc5de22
ohby00 <quddnr217@naver.com>
2024-03-31 13:32:43 +0900

    회원가입 페이지

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import AppBar from './component/Appbar';
+import User from './component/User';
 
 function App() {
   return (
     <div className='App'>
       <AppBar/>
+      <User/>
     </div>
   );
 }
