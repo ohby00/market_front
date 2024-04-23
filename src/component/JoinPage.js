216ec3beb07bc750e982bb8c373597ed83232859
ohby00 <quddnr217@naver.com>
2024-04-23 18:05:43 +0900

    이메일 인증 해결

--- src/component/JoinPage.js
+++ src/component/JoinPage.js
@@ -26,17 +26,31 @@ export default function JoinPage() {
     }
   };
 
-  const handleVerification = async () => {
+  const handleVerification = async (e) => {
+    e.preventDefault();
+    const verifyData = { email, verificationCode }; // email과 verificationCode를 verifyData 객체에 담음
+    const config = {
+        method: 'post', // POST 메서드
+        url: 'http://localhost:8080/user/checkEmail', // 요청 URL
+        data: verifyData, // verifyData를 요청 데이터로 설정
+        headers: {
+            'Content-Type': 'application/json', // JSON 형식으로 데이터 전송
+          
+        },
+    };
     try {
-      const response = await axios.post('http://localhost:8080/user/checkEmail', { email, Code });
-      console.log(response.data)
-      alert('이메일 인증이 완료되었습니다.');
-      navigate('/');
+        const response = await axios(config); // axios를 사용하여 POST 요청 전송
+        console.log(email);
+        console.log(verificationCode);
+        alert('이메일 인증이 완료되었습니다.');
     } catch (error) {
-      console.error('이메일 인증 오류:', error);
-      alert('인증 코드가 일치하지 않습니다.');
+        console.error('이메일 인증 오류:', error);
+        console.log(email);
+        console.log(verificationCode);
+        alert('인증 코드가 일치하지 않습니다.');
     }
-  };
+};
+
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -95,7 +109,7 @@ export default function JoinPage() {
                 variant="contained"
                 onClick={handleVerification}
               >
-                확인
+                인증 코드 확인 
               </Button>
             </div>
 
